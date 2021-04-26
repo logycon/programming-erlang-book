7da4b933a7feaacd176b7a8b1e6a75a755da1f44
Igor <igor@logycon.com>
2021-04-26 17:52:38 -0400

    geometry updates

--- src/ch4/geometry1.erl
+++ src/ch4/geometry1.erl
@@ -1,11 +1,21 @@
 -module(geometry1).
--export([test/0, area/1]).
+-export([test/0, area/1, perimeter/1]).
 
 area({rectangle, Width, Height}) -> Width * Height;
 area({square, Side}) -> Side * Side;
-area({circle, Radius}) -> Radius * Radius * math:pi().
+area({circle, Radius}) -> Radius * Radius * math:pi();
+area({'right triangle', K1, K2}) -> K1 * K2 / 2.
+
+perimeter({rectangle, Width, Height}) -> Width * 2 + Height * 2;
+perimeter({square, Side}) -> Side * 4;
+perimeter({circle, Radius}) -> 2 * math:pi() * Radius;
+perimeter({'right triangle', K1, K2}) -> K1 + K2 + hypot(K1, K2).
+
+hypot(K1, K2) -> math:sqrt(K1*K1 + K2*K2).
 
 test() ->
   12 = area({ rectangle, 3, 4 }),
   144 = area({ square, 12 }),
+  16.0 = area({'right triangle', 8, 4}),
+  5.0 = hypot(3, 4),
   tests_worked.
