60420b5fece3303fc599ab57e166a7e9424e30d1
Igor <igor@logycon.com>
2021-05-02 07:22:55 -0400

    ascii table

---
+++ src/ch7/ascii.erl
@@ -0,0 +1,20 @@
+-module(ascii).
+-export([print/0]).
+
+for(Max, Max, F) -> [F(Max)];
+for(Index, Max, F) -> [F(Index) | for(Index + 1, Max, F)].
+
+print() ->
+  io:format("Dec \t Char \t Hex \t Oct \t Bin ~n"),
+  io:format("------------------------------------------ ~n"),
+  F = fun(N) -> io:format("~p \t ~p \t ~p \t ~p \t ~p ~n",
+    [
+      integer_to_list(N, 10),
+      io_lib:format("~c", [N]),
+      integer_to_list(N, 16),
+      integer_to_list(N, 8),
+      integer_to_list(N, 2)
+    ]
+  ) end,
+  for(0, 127, F),
+  done.
