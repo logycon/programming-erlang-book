386b0863a488e3710a12ab89a4c2eaeb740d67f7
Igor <igor@logycon.com>
2021-05-02 07:58:01 -0400

    pad binary with zeros

--- src/ch7/ascii.erl
+++ src/ch7/ascii.erl
@@ -7,13 +7,14 @@ for(Index, Max, F) -> [F(Index) | for(Index + 1, Max, F)].
 print() ->
   io:format("Dec \t Char \t Hex \t Oct \t Bin ~n"),
   io:format("------------------------------------------ ~n"),
+
   F = fun(N) -> io:format("~p \t ~p \t ~p \t ~p \t ~p ~n",
     [
       integer_to_list(N, 10),
       io_lib:format("~c", [N]),
       integer_to_list(N, 16),
       integer_to_list(N, 8),
-      integer_to_list(N, 2)
+      string:right(integer_to_list(N, 2), 8, $0)
     ]
   ) end,
   for(0, 127, F),
